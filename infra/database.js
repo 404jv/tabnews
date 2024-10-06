04f38847b2180f6cb5652d28c317e68aca6c8336
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-10-06 22:58:02 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,20 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+  console.log({
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+  });
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error)    
+    throw error;
   } finally {
     await client.end();
   }
