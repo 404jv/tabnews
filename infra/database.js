d12eb86524bd41a6aa42723fff300ed19a5f3520
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-10-14 14:50:52 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === 'development' ? false : true,
-  });
-  console.log({
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -31,3 +24,12 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "development" ? false : true;
+}
