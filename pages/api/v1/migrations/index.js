6c0de43d098e4fa8dc88b71ea4a37549d0df0e3e
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-11-28 22:30:23 +0000

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,11 @@
 import migrationRunner from 'node-pg-migrate'
 import { join } from 'node:path'
+import database from 'infra/database'
 
 export default async function status(request, response) {
+  const dbClient = await database.getNewClient()
   const defaultMigrationsOptions = {
+    dbClient,
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join('infra', 'migrations'),
@@ -12,6 +15,7 @@ export default async function status(request, response) {
   }
   if (request.method === 'GET') {
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions)
+    await dbClient.end()
     return response.status(200).json(pendingMigrations)
   }
   if (request.method === 'POST') {
@@ -19,6 +23,7 @@ export default async function status(request, response) {
       ...defaultMigrationsOptions,
       dryRun: false,
     })
+    await dbClient.end()
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations)
     }
